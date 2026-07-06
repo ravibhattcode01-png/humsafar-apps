8db7c91342ade054ebde94582fcee2ed53f0de06
ravibhattcode01-png <ravibhattcode01@gmail.com>
2026-07-06 19:11:02 +0530

    Update baseUrl to production URL

--- humsafar_rider/lib/config.dart
+++ humsafar_rider/lib/config.dart
@@ -4,7 +4,7 @@ class AppConfig {
   /// Laravel backend base URL (bina trailing slash).
   /// Local test:  http://10.0.2.2:8000  (Android emulator -> host machine)
   /// Production:  https://api.humsafar.app
-  static const String baseUrl = 'http://10.0.2.2:8000';
+  static const String baseUrl = 'https://humsafar.ngie.in';
 
   static const String apiUrl = '$baseUrl/api/v1';
 
